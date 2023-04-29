741c22f79326d9c10bf851b702c8a6f628f19442
N0GN0l <danielzhou323@gmail.com>
2023-04-29 18:24:16 -0400

    Update mass translate.py

--- mass translate.py
+++ mass translate.py
@@ -4,14 +4,15 @@ from tkinter import *
 
 languages = []
 root = Tk()
-translator = Translator()
+translater = Translator()
+
 
 # Creating a label widget
-label = Label(root, text="Hello world")
-language = Label(root, text="Chinese")
+# label = Label(root, text="Hello world")
+# language = Label(root, text="Chinese")
 # Shoving it onto the screen
-label.grid(row=0, column=0)
-language.grid(row=1, column=5)
+# label.grid(row=0, column=0)
+# language.grid(row=1, column=5)
 
 
 # Create Chinese button function
@@ -67,8 +68,67 @@ italianChoose.grid(row=15, column=35)
 def outputEmail():
     for i in languages:
         if i == 1:
-            chineseOutput = Label(root, text=translator.translate(email.get(), src="en", dest="chinese (simplified)"))
-            chineseOutput.grid(row=30, column=30)
+            china = Toplevel()
+            china.title("Chinese email")
+            china.geometry("1000x100")
+            def focusText(event):
+                chineseOutput.config(state='normal')
+                chineseOutput.focus()
+                chineseOutput.config(state='disabled')
+            chineseOutput = Text(china, borderwidth=0)
+            chineseOutput.insert(1.0, translater.translate(email.get(), src="en", dest="zh-cn").text)
+            chineseOutput.pack()
+
+            chineseOutput.configure(state="disabled")
+
+            chineseOutput.bind('<Button-1>', focusText)
+        if i == 2:
+            spain = Toplevel()
+            spain.title("Spanish email")
+            spain.geometry("1000x100")
+            def focusText(event):
+                spanishOutput.config(state='normal')
+                spanishOutput.focus()
+                spanishOutput.config(state='disabled')
+            spanishOutput = Text(spain, borderwidth=0)
+            spanishOutput.insert(1.0, translater.translate(email.get(), src="en", dest="es").text)
+            spanishOutput.pack()
+
+            spanishOutput.configure(state="disabled")
+
+            spanishOutput.bind('<Button-1>', focusText)
+        if i == 3:
+            def focusText(event):
+                frenchOutput.config(state='normal')
+                frenchOutput.focus()
+                frenchOutput.config(state='disabled')
+
+            france = Toplevel()
+            france.title("French Email")
+            france.geometry("1000x100")
+            frenchOutput = Text(france, borderwidth=0)
+            frenchOutput.insert(1.0, translater.translate(email.get(), src="en", dest="fr").text)
+            frenchOutput.pack()
+
+            frenchOutput.configure(state="disabled")
+
+            frenchOutput.bind('<Button-1>', focusText)
+
+        if i == 4:
+            italy = Toplevel()
+            italy.title("Italian email")
+            italy.geometry("1000x100")
+            def focusText(event):
+                italianOutput.config(state='normal')
+                italianOutput.focus()
+                italianOutput.config(state='disabled')
+            italianOutput = Text(italy, borderwidth=0)
+            italianOutput.insert(1.0, translater.translate(email.get(), src="en", dest="it").text)
+            italianOutput.pack()
+
+            italianOutput.configure(state="disabled")
+
+            italianOutput.bind('<Button-1>', focusText)
 
 
 # creating a button for output
@@ -76,7 +136,7 @@ output = Button(root, text="Output email", padx=20, pady=10, bg="grey", command=
 output.grid(row=35, column=20)
 
 # Creating input widget
-email = Entry(root)
-email.grid(row=0, column=0)
+email = Entry(root, width=50, borderwidth=10)
+email.grid(row=0, column=20)
 
 root.mainloop()
