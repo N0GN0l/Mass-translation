5073fea55cb54af81b268ce09d59404fc43c6a21
N0GN0l <86800148+N0GN0l@users.noreply.github.com>
2023-04-29 10:40:38 -0400

    bat

--- new_languages.bat
+++ new_languages.bat
@@ -1,1 +1,22 @@
-echo off
+@echo off
+set /P row= please choose a row: 
+set /P column= please choose a column: 
+set /P input= please choose a language: 
+set /P number= please input the language number: 
+echo def %input%():
+echo    languages.append(%number%)
+echo    print(languages)
+echo    %input%Choose['state'] = DISABLED
+
+echo %input%Choose = Button(root, text="%input%?", padx=20, command=%input%, bg="grey",)
+echo
+echo
+echo
+echo
+echo
+echo
+echo %input%Choose.grid(row=%row%, column=%column%)
+
+
+
+pause
