b18e96bbc2ed9c53cf1438ea07ec25d4a519fffb
N0GN0l <danielzhou323@gmail.com>
2023-04-29 18:28:31 -0400

    Update new_languages.bat

--- new_languages.bat
+++ new_languages.bat
@@ -2,21 +2,26 @@
 set /P row= please choose a row: 
 set /P column= please choose a column: 
 set /P input= please choose a language: 
+set /p country= please input country of origin: 
+set /P languageModule= please input the language as a usable module for google translate
 set /P number= please input the language number: 
 echo def %input%():
 echo    languages.append(%number%)
 echo    print(languages)
 echo    %input%Choose['state'] = DISABLED
-
 echo %input%Choose = Button(root, text="%input%?", padx=20, command=%input%, bg="grey",)
-echo
-echo
-echo
-echo
-echo
-echo
 echo %input%Choose.grid(row=%row%, column=%column%)
-
-
-
+echo        if i == %number%:
+echo            %country% = Toplevel()
+echo            %country%.title("%language% email")
+echo            %country%.geometry("1000x100")
+echo            def focusText(event):
+echo                %language%Output.config(state='normal')
+echo                %language%Output.focus()
+echo                %language%Output.config(state='disabled')
+echo            %language%Output = Text(china, borderwidth=0)
+echo            %language%Output.insert(1.0, translater.translate(email.get(), src="en", dest="%languageModule%").text)
+echo            %language%Output.pack()
+echo            %language%Output.configure(state="disabled")
+echo            %language%Output.bind('<Button-1>', focusText)
 pause
